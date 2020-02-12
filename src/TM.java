fca29b132ab057a52011e83d0883cecf7f15fbbb
6135 <guibscosta@hotmail.com>
2020-02-12 18:02:17 +0000

    3

--- src/TM.java
+++ src/TM.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Iterator;
-tesdt2
+tesdt23
 public class TM {
 	private Collection<String> s;
 	private Hashtable<String,String> tokensMap = new Hashtable<String,String>();
